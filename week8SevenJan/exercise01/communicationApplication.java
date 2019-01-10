ed5fe5f3a776af4ee2f892ad6376640f88ee2c48
tina-fernan <tinaaroom@gmail.com>
2019-01-10 20:08:50 +0100

    Robomime

--- week8SevenJan/exercise01/communicationApplication.java
+++ week8SevenJan/exercise01/communicationApplication.java
@@ -2,19 +2,17 @@ package week8SevenJan.exercise01;
 
 import excercise01.FileReader;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class communicationApplication {
 
     public static void main(String[] args) {
-        FileReader reader=new FileReader();
-        Sensor sensor=new Sensor();
+        FileReader reader = new FileReader();
+        Sensor sensor = new Sensor();
         Archieve archieve = new Archieve();
 
-       List<String>line=reader.asLines("week8SevenJan/exercise01/robomime.txt ");
-       List<String>trick = sensor.decryptTrick(line);
+        List<String> lines = reader.asLines("week8SevenJan/exercise01/robomime.txt");
+        List<String> trick = sensor.decryptTrick(lines);
 
         archieve.getTrick(trick);
 
